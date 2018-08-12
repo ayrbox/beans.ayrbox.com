349785b168ed1b0276eba116a663f6c830810339
Sabin Raj DANGOL <sawbean.raz@googlemail.com>
2018-08-12 13:46:41 +0100

    upate about me page

--- src/pages/aboutme.js
+++ src/pages/aboutme.js
@@ -5,18 +5,103 @@ const SecondPage = () => (
   <div>
     <h1>About me</h1>
     <p>
-      I am NodeJS Developer @{" "}
-      <a href="https://appliedblockchain.com/">Applied Blockchain</a>
+      My name is Sabin and I am NodeJS Developer @{" "}
+      <a href="https://appliedblockchain.com/">Applied Blockchain</a>.
+    </p>
+    <p>
+      Curious and Self-motivated full stack developer and a technology
+      enthusiast. I believe and passionate about open source. I try not to
+      re-invent wheel and follow DRY approach. Good enough is better than
+      Perfect but there is always room for improvement and innovation.
     </p>
 
-    <h3 className="mb-3 mt-5">Personal Summary</h3>
+    <h4>Work Experience so far</h4>
+
+    <h5>
+      Applied Blockchain <small>NodeJS Backend Developer</small>
+    </h5>
     <p>
-      I am self-motivated curious full stack developer and a technology
-      enthusiast. I am strong believer of open source. Developers are lazy so am
-      I, so I follow DRY approach. I believe there is always room for
-      improvement so I continuously seek innovative ways of doing things.
-      developer, I seek for better solution for every problem.
+      Joining Applied Blockchain from today and super excited and eagar to
+      learn. NodeJS and eco system where I thrived in recent year. Got the
+      change to put my knowledger and experience in action. I am sure to learn
+      more and give it back to company.
     </p>
+    <ul>
+      <li>NodeJS</li>
+      <li>React</li>
+      <li>Ethereum Blockchain</li>
+      <li>Ruby</li>
+    </ul>
+    <h5>
+      Giant Precision Ltd <small>Software Developer</small>
+    </h5>
+    <p>
+      Longest exprience of all as is with Giant Precision which is part of much
+      bigger Giant Group. In short, I was in giant as graduate developer and
+      came out as much experienced software developer.
+    </p>
+
+    <p>
+      Experience was much of roller-coaster as the company is huge, lots of
+      projects, number of stakeholder. Giant never disappointed me as employer
+      and always supportive.
+    </p>
+    <p>
+      In almost 6 years of exprience, I gained lots of technical knowledge, and
+      opportunity to implement and test out in real world of software
+      engineering.
+    </p>
+    <ul>
+      <li>C#</li>
+      <li>SQL</li>
+      <li>Asp.net</li>
+      <li>AngularJs</li>
+      <li>ASP</li>
+      <li>Sass</li>
+    </ul>
+    <h5>
+      Ethos Technologies Limited <small>Developer</small>
+    </h5>
+    <p>
+      Ethos gave me change to work on their project even though it were an Adhoc
+      Project. Not a long time but definitely help gain confidence that I am
+      able to work in in language and framework if I put my focus and time.
+    </p>
+    <ul>
+      <li>PHP</li>
+      <li>Wordpress</li>
+      <li>Joomla</li>
+      <li>Codeigniter</li>
+      <li>MySQL</li>
+    </ul>
+
+    <h5>
+      Gapsco Int’l Pvt. Ltd. <small>Programmer</small>
+    </h5>
+    <p>
+      Right after my high-school I joined gapsco as a Programmer where I learn
+      lots skills that I tresured. The team was incredible group of people.
+    </p>
+    <p>
+      I learned how to communicate with people. Co-Worker and Client. The help
+      me be better communicator. Communication is vital as software developer
+      has there is lot more human factor that computure and code. Object
+      Oriented Programming (OOP), I first got a taste of OOP while working in
+      gapsco. I did not realised that simple 1 hours tutorial would have that
+      impact in entire career.
+    </p>
+    <p>Exprience in gapsco is the paving stone in terms of my career</p>
+
+    <ul>
+      <li>Visual Basic</li>
+      <li>C#</li>
+      <li>WPF</li>
+      <li>MS SQL</li>
+      <li>MS Access</li>
+    </ul>
+
+    {/* <h4>Skills</h4> */}
+
     <Link to="/">Back</Link>
   </div>
 );
